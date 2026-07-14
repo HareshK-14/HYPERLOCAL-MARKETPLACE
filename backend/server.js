88a303d81998de7a004df852b3820cb17d9f6a1c
HareshK-14 <hareeshraj14@gmail.com>
2026-07-14 17:56:41 +0530

    debug: add password length check

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_SSL:', process.env.DB_SSL);
+console.log('DB_PASSWORD length:', process.env.DB_PASSWORD?.length);
+console.log('DB_PASSWORD first4:', process.env.DB_PASSWORD?.substring(0, 4));
 
 const app = express();
 const server = http.createServer(app);
